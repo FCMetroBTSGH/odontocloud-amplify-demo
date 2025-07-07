2cfa6ba5ce294a13c4ff71ad0c76988fac2e4b6c
FCMetroBTSGH <fchoudhary@metrobts.com>
2025-07-07 13:08:52 -0400

    Add files via upload

--- db_001_init.sql
+++ db_001_init.sql
@@ -1,7 +1,4 @@
--- minimal seed: specialties and procedures
 create extension if not exists "uuid-ossp";
-
 insert into specialties(id, code, name) values
   (uuid_generate_v4(),'PERIO','Periodontics'),
   (uuid_generate_v4(),'OS','Oral Surgery');
--- add more manually for demo
